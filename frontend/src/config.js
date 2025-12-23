0671eed3db7c8b1be3b66ae348a4a99846f02d21
saif akhtar <saifakhtar285@gmail.com>
2025-12-23 14:48:11 +0530

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,4 @@
-// src/config.js
-export const API_BASE_URL = "https://movie-management-app-pqcy.onrender.com";
-git init
-git add .
-git commit -m "Frontend ready for Netlify deployment"
-git branch -M main
 
-git push -u origin main
+
+// Base URL of your backend on Render
+export const API_BASE_URL = "https://movie-management-app-pqcy.onrender.com";
