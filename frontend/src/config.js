abb5162294c948026b804a7a7d1b75ec34815440
saif akhtar <saifakhtar285@gmail.com>
2025-12-23 14:46:41 +0530

    Add new files / updates

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+// src/config.js
+export const API_BASE_URL = "https://movie-management-app-pqcy.onrender.com";
+git init
+git add .
+git commit -m "Frontend ready for Netlify deployment"
+git branch -M main
+
+git push -u origin main
